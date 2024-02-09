87d259410ac12ba25c0cef86f40d312b5bd342cd
Yogesh Sewada <yogeshsewada0@gmail.com>
2024-02-09 23:02:04 +0530

    first commit

---
+++ clothsorder/components/categories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Categories() {
+  return (
+    <div className="border-b border-[#D9D9D9] pb-2 pt-8 pl-8">
+      <ul className="flex text-[#944E63]">
+        <li className="mr-5">Men</li>
+        <li className="mr-5">Women</li>
+        <li className="mr-5">Children</li>
+        <li>Dolls</li>
+      </ul>
+    </div>
+  );
+}
